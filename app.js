77e044a9a0d7c842b028667cedfe187b7dc64c45
ugurakcora <uakcora97@gmail.com>
2023-10-30 14:18:21 +0300

    init perspective playground

---
+++ app.js
@@ -0,0 +1,40 @@
+Vue.createApp({
+  data() {
+    return {
+      perspective: 100,
+      rotateX: 0,
+      rotateY: 0,
+      rotateZ: 0,
+    };
+  },
+  computed: {
+    boxStyle() {
+      return {
+        transform: `
+          perspective(${this.perspective}px)
+          rotateX(${this.rotateX}deg)
+          rotateY(${this.rotateY}deg)
+          rotateZ(${this.rotateZ}deg)`,
+      };
+    },
+  },
+  methods: {
+    reset() {
+      this.perspective = 100;
+      this.rotateX = 0;
+      this.rotateY = 0;
+      this.rotateZ = 0;
+    },
+    copy() {
+      let text = `transform: ${this.boxStyle.transform};`;
+      navigator.clipboard
+        .writeText(text)
+        .then(() => {
+          alert("Copied to clipboard");
+        })
+        .catch((err) => {
+          alert("Failed to copy to clipboard");
+        });
+    },
+  },
+}).mount("#app");
